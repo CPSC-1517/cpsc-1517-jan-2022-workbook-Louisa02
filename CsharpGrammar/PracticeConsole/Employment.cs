0d70593f182bd61b6660dde4f3279afd18c30e8a
Louisa02 <louisaluo702@gmail.com>
2022-01-17 16:24:25 -0700

    added new notes to Employment.cs

--- CsharpGrammar/PracticeConsole/Employment.cs
+++ CsharpGrammar/PracticeConsole/Employment.cs
@@ -20,7 +20,7 @@ namespace Practice.Data
         // behaviour (method)
 
 
-        // data field
+        // Data field
         // are storage area in your class
         // these are treated as variables
         // these may be public, private, public readonly
@@ -28,11 +28,11 @@ namespace Practice.Data
         private string _Title;
         private double _Years;
 
-        // properties
+        // Properties
         // These are acess techniques to retrieve or set data in 
         // your class without directly touching the storage data field
 
-        // fully implemented property
+        // Fully-implemented property
         // a) a declared storage data (data field)
         // b) a declared property signature
         // c) a coded get "method"
@@ -67,21 +67,21 @@ namespace Practice.Data
                 {
                     throw new ArgumentNullException("Title is a required piece of data.");
                 }
-                else
-                {
-                    _Title = value;
-                }
+
+                // data is considered valid
+                _Title = value;
             }
         }
 
-        // auto-implemented property
+        // Auto-implemented property
         // These properties differ only in syntax
         // Each property is responsible for a single piece of data
         // These properties do NOT reference a declared private data memver
-        // The system generates an internal storage area of the return data typee
+        // The system generates an internal storage area of the return data type
+        // The system manages the internal storage for the accessor and mutator
         // there is NO additional logic applied to the data value
-
         public int Level { get; set; }
+
 
         // This property Years could be coded as either a fully implemented property
         // or as an auto-implemented property
@@ -93,11 +93,72 @@ namespace Practice.Data
         }
 
 
-        // constructors
+        // Constructors
+        // is to initialize the physical object (instance) during its creation
+        // the result of creation is ensure that the coder gets an instance in 
+        // a known state
+        // if ypur class definition has NO constructor coded, then the data members /
+        // auto implemented properties are set to the C# default data type value
+        
+        // You can code one or more constructors in your class definition
+        // IF YOU CODE A CONSTRUCTOR FOR THE CLASS, YOU ARE RESPONSIBLE FOR ALL
+        // CONSTRUCTORS USED BY THE CLASS!
 
+        // Generally, if you are going to code your own constructor(s) you code two types
+        // Default: this constructor does NOT take in any parameters (it mimics the default system)
+        // Greedy: this constructor has list of parameters, one for each property, declare
+        //          for incoming data
 
+        // Syntax: accesstype classname([list of parameters]) { constructor code body}
 
-        // behaviours
+        // IMPORTANT: The constructor DOES NOT have a return datatype
+        // You DO NOT call a constructor directly, called using the new operator
+
+        // Default Constructor
+        public Employment()
+        {
+            // constructor body
+            // a) empty
+            // b) you COULD assign literal value to your properties with this constructor
+            Level = 0;
+            Title = "Unknown";
+        }
+
+        // Greedy Constructor
+        public Employment(string title, int level, double years)
+        {
+            // constructor body
+            // a) a parameter for each property
+            // b) you COULD do validation within the constructor instead of the property
+            // c) validation for public readonly data members
+            //    validation for a property with a private set
+            Title = title;
+            Level = level;
+            Years = years;
+        }
+        // Behaviours (aka methods)
+        // Behaviours are no different than methods elsewhere
+
+        //Syntax: accesstype [static] returndatatype BehaviourName ([list of parameters])
+        // { code body}
+
+        // there maybe times you wish to obtain all the data in your instance
+        // all at once for display
+        // generally to accomplish this, your class overrides the .ToString() method
+
+        public override string ToString()
+        {
+            // comma seperate value string (csv)
+            return $"{Title},{Level},{Years}";
+        }
+
+        public void SetEmployeeResponsibilityLevel(int level)
+        {
+            // you could do validation within this method to ensure acceptable value
+            if (level < 0)
+                throw new Exception("Responsibility Level must be positive.");
+            Level = level;
+        }
     }
 }
 
