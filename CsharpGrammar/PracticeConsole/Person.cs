a8e2b5fa5c9d4bb3f42cb88711e49a496bfbaf32
Louisa02 <louisaluo702@gmail.com>
2022-01-18 09:44:08 -0700

    Added a new class Person.cs

---
+++ CsharpGrammar/PracticeConsole/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsole.Data
+{
+    public class Person
+    {
+        // example of a composite class
+        // each instance of this class will represent an individual
+        // This class will define the following characteristics of a person
+        // First Name, Last Name, the current resident address, list of employment positions
+
+        private string _FirstName;
+        private string _LastName;
+
+        public string FirstName
+        {
+            get 
+            { 
+                return _FirstName; 
+            }
+            set
+            {
+                if(Utilities.IsEmpty(value))
+                {
+                    throw new ArgumentNullException("First name is required.");
+                }
+                _FirstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _LastName;
+            }
+            set
+            {
+                if (Utilities.IsEmpty(value))
+                {
+                    throw new ArgumentNullException("Last name is required.");
+                }
+                _LastName = value;
+            }
+        }
+
+        public ResidentAddress Address;
+
+        public List<Employment> EmploymentPositions { get; set; }
+
+        public Person()
+        {
+            // if an instance of List<T> is not created and assigned then
+            // the property will have an initial value of null
+
+            EmploymentPositions = new List<Employment>();
+        }
+
+      
+
+    }
+}
