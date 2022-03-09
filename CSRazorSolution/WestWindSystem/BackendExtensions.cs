fda94a514b07fe5f997cf4a4b1b06823778d8844
Louisa02 <Louisaluo702@gmail.com>
2022-03-09 09:41:35 -0700

    Create BuildVersionServices in BLL and register services

--- CSRazorSolution/WestWindSystem/BackendExtensions.cs
+++ CSRazorSolution/WestWindSystem/BackendExtensions.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 #region Additional Namespaces
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using WestWindSystem.BLL;
 using WestWindSystem.DAL;
 #endregion
 namespace WestWindSystem
@@ -21,6 +22,23 @@ namespace WestWindSystem
 
             //setup the context service
             services.AddDbContext<WestWindContext>(options);
+
+            //register the service class
+
+            //add any business logic layer class to the service collection so our
+            //  web app has access to the methods (services) within the BLL class
+
+            //the argument for the AddTrainsient is called a factory
+            //basically what you are add is a localize method
+            services.AddTransient<BuildVersionServices>((serviceProvider) =>
+            {
+                //get the dbcontext class that has been registered
+                var context = serviceProvider.GetService<WestWindContext>();
+                //create an instance of the service class (BuildVersionServices) supplying
+                //   the context reference to the service class
+                //return the service class instance
+                return new BuildVersionServices(context);
+            });
         }
     }
 }
